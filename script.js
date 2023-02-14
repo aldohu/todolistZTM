66c1d87b8921fecce0d5a5e82354f6785e7e7a33
Aldin <alhuric89@gmail.com>
2023-02-14 13:09:06 +0100

    remove unsed code

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// var button = document.getElementById("enter");
-// var input = document.getElementById("userinput");
-// var ul = document.querySelector("ul");
-
-// function inputLength() {
-// 	return input.value.length;
-// }
-
-// function createListElement() {
-// 	var li = document.createElement("li");
-// 	li.appendChild(document.createTextNode(input.value));
-// 	ul.appendChild(li);
-// 	input.value = "";
-// }
-
-// function addListAfterClick() {
-// 	if (inputLength() > 0) {
-// 		createListElement();
-// 	}
-// }
-
-// function addListAfterKeypress(event) {
-// 	if (inputLength() > 0 && event.keyCode === 13) {
-// 		createListElement();
-// 	}
-// }
-
-// button.addEventListener("click", addListAfterClick);
-
-// input.addEventListener("keypress", addListAfterKeypress);
 
 
 let ul = document.querySelector("ul");
@@ -59,22 +29,7 @@ function addTodoButton() {
 }
 function addTodoKeyPress(e) {
 	if(input.value.length > 0 && e.key === "Enter"){
-	// 	let li = document.createElement("li");
-	// 	li.textContent = input.value;
-	// 	ul.append(li)
-		
-	// 	let button = document.createElement("button");
-    // 	button.textContent = "delete";
-    // 	li.append(button);
-	// 	li.addEventListener("click",function(){
-	// 		li.classList.toggle("done");
-	// 	})
-    // 	button.addEventListener("click", function (event) {
-    //   	let clickedButton = event.target;
-    //   	clickedButton.parentNode.remove();
-		
-	// 	});
-	// input.value = ""
+	
 	createLi(e);	
 }
 }
@@ -98,36 +53,3 @@ input.addEventListener("keypress", addTodoKeyPress);
  }
 
 
-// function addTodoButton() {
-//   if (input.value.length > 0) {
-//     let li = document.createElement("li");
-//     li.textContent = input.value;
-//     ul.append(li);
-
-//     // Add a delete button to the new li element
-//     let button = document.createElement("button");
-//     button.textContent = "delete";
-//     li.append(button);
-
-//     button.addEventListener("click", function (event) {
-//       let clickedButton = event.target;
-//       clickedButton.parentNode.remove();
-//     });
-
-//     input.value = "";
-//   }
-// }
-
-// addButton.addEventListener("click", addTodoButton);
-//input.addEventListener("keypress", addTodoKeyPress);
-
-// Add a delete button to each existing li element
-// for (let i = 0; i < lis.length; i++) {
-//   let button = document.createElement("button");
-//   button.textContent = "delete";
-//   lis[i].append(button);
-//   button.addEventListener("click", function (event) {
-//     let clickedButton = event.target;
-//     clickedButton.parentNode.remove();
-//   });
-// }
